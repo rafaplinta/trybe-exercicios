2756cf235b45ba1068671dff45343e1534b48cd1
Rafaela Plinta <ra.plinta@gmail.com>
2023-01-24 10:47:13 -0300

    Exercícios JavaScript

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/exercicios-praticos/aula.js
@@ -0,0 +1,30 @@
+// 1. Hora de fazermos em conjunto! 🏄‍♀️
+// Agora vamos ter alguns minutos para construir um algoritmo com a seguinte descrição:
+// Crie dois valores num1 e num2.
+// Se os valores forem iguais some os dois
+// Caso contrário multiplique num1 por num2.
+// Por ultimo exiba o valor final no console.
+// */
+
+// let num1 = 10;
+// let num2 = 10;
+
+// if (num1 === num2) {
+//     console.log(num1 + num2);
+// } else {
+//     console.log(num1 * num2);
+// }
+
+// 2.Utilize if/else para escrever um código que retorne o maior de dois números. 
+// Defina, no começo do seu código, duas constantes com os valores que serão comparados.
+
+const num1 = 20;
+const num2 = 20;
+
+if (num1 > num2) {
+    console.log("Número 1 é maior");
+} else if (num1 < num2) {
+    console.log("Número dois é maior");
+} else {
+    console.log("Os números são iguais!")
+}
---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/exercicios-praticos/scripts.js
@@ -0,0 +1,109 @@
+
+// const constanteA = 25;
+// const constanteB = 37;
+
+// if (constanteA > constanteB) {
+//     console.log(constanteA);
+// } else {
+//     console.log(constanteB);
+// } 
+
+// const constante1 = 102;
+// const constante2 = 80;
+// const constante3 = 165;
+
+// if (constante1 > constante2 && constante1 > constante3) {
+//     console.log(constante1);
+// } else if (constante2 > constante1 && constante2 > constante3) {
+//     console.log(constante2);
+// } else {
+//     console.log(constante3);
+// }
+
+// let variavel = -10;
+
+// if (variavel >= 1){
+//     console.log("positive");
+// } else if (variavel <= -1) {
+//     console.log("negative");
+// } else {
+//     console.log("zero");
+// } 
+
+//  const anguloUm = -50;
+//  const anguloDois = 50;
+//  const anguloTres = 80;
+
+//  const somaAngulos = anguloUm + anguloDois + anguloTres;
+
+//  if (somaAngulos == 180 && anguloUm >= 0 && anguloDois >= 0 && anguloTres >= 0) {
+//     console.log(true);
+//  } else {
+//     console.log(false);
+//  }
+
+// const chess = "King";
+
+// switch (chess.toLowerCase()) {
+//     case "knight":
+//         console.log("Move-se em L");
+//         break;
+
+//     case "king":
+//         console.log("Move-se em linha reta");
+//         break;
+
+//     case "bishop":
+//         console.log("Move-se em diagonal");
+//         break;
+// }
+
+// let percent = 99;
+
+// if (percent >= 90) {
+//     console.log("A");
+// } else if (percent >= 80){
+//     console.log("B");
+// } else if (percent >= 70) {
+//     console.log("C");
+// } else if (percent >= 60) {
+//     console.log("D");
+// } else if (percent >= 50) {
+//     console.log("E");
+// } else {
+//     console.log("F");
+// }
+
+// const num1 = 5;
+// const num2 = 8;
+// const num3 = 11;
+
+// if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 ===0) {
+//     console.log(true);
+// } else {
+//     console.log(false)
+// }
+
+// const n1 = 9;
+// const n2 = 7;
+// const n3 = 3;
+
+// if (n1 % 2 === 0 || n2 % 2 === 0 || n3 % 2 === 0) {
+//     console.log(false);
+// } else {
+//     console.log(true);
+// }
+
+// const custo = 10;
+// const venda = 25;
+
+// if (custo >= 0 && venda >= 0) {
+//     const custoTotal = custo * 1.2;
+//     const lucro = (venda - custoTotal) * 1000;
+//     console.log(lucro)
+// }else {
+//     console.log("Erro")
+// }
+
+
+
