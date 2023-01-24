40875decc4c23c902d1e379fb7b118d637e4b945
Rafaela Plinta <ra.plinta@gmail.com>
2023-01-24 14:14:43 -0300

    Exercício INSS

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/exercicios-praticos/scripts.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/exercicios-praticos/scripts.js
@@ -105,5 +105,35 @@
 //     console.log("Erro")
 // }
 
+let inss;
+let aliquotaIr;
+const salarioBruto = 3000;
 
+if (salarioBruto <= 1556.94) {
+    inss = salarioBruto * 0.08;
+} else if (salarioBruto <= 2594.92) {
+    inss = salarioBruto * 0.09;
+} else if (salarioBruto <= 5189.82) {
+    inss = salarioBruto * 0.11
+} else {
+    inss = 570.88;
+}
+
+const salarioBase = salarioBruto - inss;
+console.log(salarioBase)
+
+if (salarioBase <= 1903.98) {
+    aliquotaIr = 0;
+} else if (salarioBase <= 2826.65) {
+    aliquotaIr = (salarioBase * 0.075) - 142.80; 
+} else if (salarioBase <= 3751.05 ) {
+    aliquotaIr = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase <= 4664.68) {
+    aliquotaIr = (salarioBase * 0.225) - 636.13;
+} else {
+    aliquotaIr = (salarioBase * 0.275) - 869.36;
+}
+
+console.log(aliquotaIr);
+console.log("O salário é de " + (salarioBase - aliquotaIr));
 
