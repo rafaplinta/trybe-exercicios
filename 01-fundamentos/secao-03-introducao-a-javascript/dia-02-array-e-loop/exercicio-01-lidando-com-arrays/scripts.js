356ca5d734d9f4145d4667904b006c103f0e5b33
Rafaela Plinta <ra.plinta@gmail.com>
2023-01-25 14:19:51 -0300

    Adiciona alterações

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-loop/exercicio-01-lidando-com-arrays/scripts.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-loop/exercicio-01-lidando-com-arrays/scripts.js
@@ -43,17 +43,24 @@ let soma = 0;
 //     console.log(impares);
    
   
-let array =[]; 
+// let array =[]; 
 
-for (let index = 0; index <= 25; index += 1) {
-    array.push(index);
-}
-console.log(array);
+// for (let index = 0; index <= 25; index += 1) {
+//     array.push(index);
+// }
+// console.log(array);
 
-for (let index = 0; index < array.length; index += 1) {
-    console.log(array[index] / 2);
-  };
+// for (let index = 0; index < array.length; index += 1) {
+//     console.log(array[index] / 2);
+//   };
 
+const n = 9;
+
+let resultado = 100;
+for (let index = 0; index <= n; index += 1) {
+  resultado -= index;
+};
+console.log(resultado);
 
 
 
