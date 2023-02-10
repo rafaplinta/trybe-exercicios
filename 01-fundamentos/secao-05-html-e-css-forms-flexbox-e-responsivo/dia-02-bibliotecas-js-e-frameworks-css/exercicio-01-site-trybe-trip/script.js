2716f9de4e3435d57d801587fee792d89d832a94
Rafaela Plinta <ra.plinta@gmail.com>
2023-02-10 12:54:49 -0300

    Adiciona o exercício de bootstrap

---
+++ 01-fundamentos/secao-05-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-js-e-frameworks-css/exercicio-01-site-trybe-trip/script.js
@@ -0,0 +1,15 @@
+function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  const clearBtn = () => {
+    const btn = document.getElementById('clear-btn');
+    btn.addEventListener('click',)
+  }
+
+window.onload = function () {
+  clearBtn();
+  const submitBtn = document.querySelector('#submit-btn');
+  submitBtn.addEventListener('click', handleSubmit);
+
+};
